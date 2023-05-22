045e37266f2aa5323148cf210704df57e84f842d
Abdoul Karim DIALLO <abdoulkarimfoly1996@gmail.com>
2023-05-22 00:01:09 +0000

    refractorisation du projet en mettant les variable d'environnement

--- src/app/departement/departement.service.ts
+++ src/app/departement/departement.service.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { IDepartement } from '../model/deparetement.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartementService {
+  api = environment.baseURL;
   constructor(private http: HttpClient) { }
 
   getAllDepartement(): Observable<IDepartement[]> {
-    return this.http.get<IDepartement[]>('http://localhost:8080/departements')
+    return this.http.get<IDepartement[]>(this.api + '/departements')
   }
   saveDepartement(departement: IDepartement): Observable<IDepartement> {
     return this.http.post<IDepartement>('http://localhost:8080/departements', departement);
--- src/app/helpers/stomp.service.ts
+++ src/app/helpers/stomp.service.ts
@@ -1,19 +1,18 @@
 import { Injectable } from '@angular/core';
 import * as SockJS from 'sockjs-client';
+import { environment } from 'src/environments/environment';
 import * as Stomp from 'stompjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StompService {
-
+  api = environment.baseURL
   constructor() { }
   private connecting: boolean = false;
   private topicQueue: any[] = [];
-
-  socket = new SockJS('http://localhost:8080/sba-websocket');
+  socket = new SockJS(this.api + '/sba-websocket');
   stompClient = Stomp.over(this.socket);
-
   subscribe(topic: string, callback: any): void {
     // If stomp client is currently connecting add the topic to the queue
     if (this.connecting) {
@@ -23,7 +22,6 @@ export class StompService {
       });
       return;
     }
-
     const connected: boolean = this.stompClient.connected;
     if (connected) {
       // Once we are connected set connecting flag to false
@@ -31,17 +29,14 @@ export class StompService {
       this.subscribeToTopic(topic, callback);
       return;
     }
-
     // If stomp client is not connected connect and subscribe to topic
     this.connecting = true;
     this.stompClient.connect({}, (): any => {
       this.subscribeToTopic(topic, callback);
-
       // Once we are connected loop the queue and subscribe to remaining topics from it
       this.topicQueue.forEach((item: any) => {
         this.subscribeToTopic(item.topic, item.callback);
       })
-
       // Once done empty the queue
       this.topicQueue = [];
     });
